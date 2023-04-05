53b1f68c1ac3b2cbd54d6264cafeb405b6ef9bd7
2980059119 <2134059281@qq.com>
2023-04-05 18:59:18 +0800

    初始化谷粒商城

---
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/GulimallWareApplication.java
@@ -0,0 +1,12 @@
+package com.atguigu.gulimall.ware;
+
+
+
+@SpringBootApplication
+public class GulimallWareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallWareApplication.class, args);
+	}
+
+}
